7fcd1157367961fef2c37bd9d03657357d5a2063
p-majchrzak <p-majchrzak>
2024-03-04 10:29:21 +0100

    Uzupełniono kod do strony StronaZegar, tak aby stworzyć funkcjonalności.

--- Zegar/Zegar/Widoki/StronaZegar.xaml.cs
+++ Zegar/Zegar/Widoki/StronaZegar.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,49 @@ namespace Zegar.Widoki
         public StronaZegar()
         {
             InitializeComponent();
+
+            dataTXT.Text = DateTime.Now.Date.ToString("dd MMMM yyyy");
+
+            Device.StartTimer(new TimeSpan(0, 0, 1), () =>
+            {
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    int godzina = DateTime.Now.Hour;
+                    int minuta = DateTime.Now.Minute;
+                    int sekunda = DateTime.Now.Second;
+
+                    string godzinaTekst = "";
+                    string minutaTekst = "";
+                    string sekundaTekst = "";
+
+                    if(godzina < 10)
+                    {
+                        godzinaTekst = "0" + godzina.ToString();
+                    }
+                    else
+                    {
+                        godzinaTekst = godzina.ToString();
+                    }
+                    if (minuta < 10)
+                    {
+                        minutaTekst = "0" + minuta.ToString();
+                    }
+                    else
+                    {
+                        minutaTekst = minuta.ToString();
+                    }
+                    if (sekunda < 10)
+                    {
+                        sekundaTekst = "0" + sekunda.ToString();
+                    }
+                    else
+                    {
+                        sekundaTekst = sekunda.ToString();
+                    }
+                    godzinaTXT.Text = godzinaTekst + ":" + minutaTekst + ":" + sekundaTekst;
+                });
+                return true;
+            });
         }
     }
 }
