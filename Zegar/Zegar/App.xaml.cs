eb95d2752dd4ea269e503a1f6a7a913f2f405f05
pjoterm606@gmail.com <p-majchrzak>
2024-03-04 09:51:30 +0100

    Utwórzono strukturę plików.

--- Zegar/Zegar/App.xaml.cs
+++ Zegar/Zegar/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Zegar.Widoki;
 
 namespace Zegar
 {
@@ -10,7 +11,7 @@ namespace Zegar
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new StronaZegar();
         }
 
         protected override void OnStart()
