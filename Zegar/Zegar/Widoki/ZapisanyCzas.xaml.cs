277b18bf28baa351bd2fae42fe95ad26e5803f7c
p-majchrzak <148012675+p-majchrzak@users.noreply.github.com>
2024-03-18 19:48:58 +0100

    Dodano plik w którym wyświetlać się będzie zapisany czas.

---
+++ Zegar/Zegar/Widoki/ZapisanyCzas.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Zegar.Widoki
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ZapisanyCzas : ContentPage
+    {
+        public ZapisanyCzas()
+        {
+            InitializeComponent();
+        }
+    }
+}
